8552be45b7af7d662f7e2d1ccc27d77fa6447824
Princess Onyiri <princessonyiri@gmail.com>
2021-03-06 22:39:57 -0500

    final

--- server.js
+++ server.js
@@ -14,19 +14,24 @@ app.get('/commands', async(req, res)=>{
                 <title>Stata Commands</title>
             </head>
             <body>
-                <h1>Stata Commands</h1>
-                <div class= 'commands'>
-                    ${commands.map(command=>(
-                        `<div class='stataCommand>
-                            <div class='box'>
-                                ${command.id}|<a href="http://localhost:3000/commands/${command.id}"> ${command.command}</a>
-                            </div>
-                            <div>
-                                ${command.link}
-                            </div>
-                        </div>`
-                    )).join('')}
-
+                <div class="logo"> 
+                <img src="https://www.stata.com/includes/images/stata-logo-release-16.svg" width="150" height = "150"> 
+                </div> 
+                <div class='middle'>
+                    <div class='commands'>
+                        <div>
+                            ${commands.map(command=>(
+                                `<div class='stataCommand'>
+                                    <div>
+                                        ${command.id}|<a href="http://localhost:3000/commands/${command.id}"> ${command.command}</a>
+                                    </div>
+                                    <div>
+                                        <a href=${command.link} target='_blank'>Click here to read command documentation</a>
+                                    </div>
+                                </div>`
+                            )).join('')}
+                        </div>
+                    </div>
                 </div>
             </body>
         </html>`)
@@ -45,26 +50,32 @@ app.get('/commands/:id', async(req, res)=>{
                 <title>${stataName[0].command}</title>
             </head>
             <body>
+                <div class="logo"> 
+	            <img src="https://www.stata.com/includes/images/stata-logo-release-16.svg" width="150" height = "150"> 
+                </div> 
                 <button type="button" onclick="location.href='http://localhost:3000/commands/';">Back</button>
-                <h1>${stataName[0].command}</h1>
-                <div class= 'commands'>
-                    ${stataName.map(command=>(
-                        `<div class='namesId box'>
-                            <div>
-                                ${command.id}| ${command.command}
-                            </div>
-                            <div>
-                                ${command.description}
-                            </div>
-                            <div>
-                                ${command.example}
-                            </div>
-                            <div>
-                                <a href='${command.link}'>Click here for stata documentation on ${command.command}</a>
-                            </div>
-                        </div>`
-                    )).join('')}
-
+                <div class= 'commanders'>
+                    <div>
+                        <div>
+                            <h1>${stataName[0].command}</h1>
+                        </div>
+                        ${stataName.map(command=>(
+                            `<div class='box'>
+                                <div class='item'>
+                                    ${command.id}| ${command.command}
+                                </div>
+                                <div class='item'>
+                                    ${command.description}
+                                </div>
+                                <div class='item'>
+                                    ${command.example}
+                                </div>
+                                <div class='item'>
+                                    <a href='${command.link}' target='_blank'>Click here for stata documentation on ${command.command}</a>
+                                </div>
+                            </div>`
+                        )).join('')}
+                </div>
                 </div>
             </body>
         </html>`)
